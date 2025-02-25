238f8a2c01ecd4e10049d7f36d00def8121d91c8
Weuzcma <ocamara5283@bronxinternationalhs.com>
2025-02-25 17:24:53 -0500

    Update SamplespeedrobotTeleop.java

--- SamplespeedrobotTeleop.java
+++ SamplespeedrobotTeleop.java
@@ -4,4 +4,14 @@
   controls to limit drift.
   /*
   package org.first.thinkingfrc.team2train
+    @TeleOp(name="Sample Teleop", group = "Mr. ousseynou")
+public class SampleTeleop extends LinearOpMode
+{
+    
+
+    final double SAFE_DRIVE_SPEED   = ?
+    final double SAFE_STRAFE_SPEED  = ?
+    final double SAFE_YAW_SPEED     = ?
+    final double HEADING_HOLD_TIME  = ?
+
   
