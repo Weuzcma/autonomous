b3e5277ec084350ab7388df86fcba1e06db655b6
Weuzcma <ocamara5283@bronxinternationalhs.com>
2025-02-25 17:20:54 -0500

    Create SamplespeedrobotTeleop.java

---
+++ SamplespeedrobotTeleop.java
@@ -0,0 +1,7 @@
+/* Created ousseynou camara. 2025.
+    This class illustrates my simplified Odometry Strategy.
+    It implements basic straight line motions but with heading and drift
+  controls to limit drift.
+  /*
+  package org.first.thinkingfrc.team2train
+  
